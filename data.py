b285f13a623ec42c75bfd9789414fca924793b96
Alex <eidos92@gmail.com>
2019-03-03 13:32:12 -0700

    Add documentation to data.py

--- data.py
+++ data.py
@@ -8,10 +8,27 @@ from joblib import Parallel, delayed
 
 
 def unit_interval_normalization(x):
+    '''
+    intensity unit interval normalization.
+
+    Arguments:
+        x (Tensor): input tensor
+    '''
     return (x - x.min()) / (x.max() - x.min())
 
 
 class Subject:
+    '''
+    Encapsulates subject data.
+
+    Arguments:
+        path (str): path to subjet folder
+        input_filenames (list of str): filenames of input volumes
+        target_filename (str): filename of target
+        subvolume_shape (array of ints): sampled subvolumes shape
+        preprocessing (list of functions): functions is used to preprocess input
+        extended (Bool): extend volume shape to be dividend of subvolume shape
+    '''
     def __init__(self, path, input_filenames, 
         target_filename, subvolume_shape, 
         preprocessing, extended):
@@ -24,7 +41,13 @@ class Subject:
         self._extended = extended
 
 
-    def _pad_volume(self, _input):
+    def _extend_volume(self, _input):
+        '''
+        Extends volume shape to be dividend of subvolume shape.
+
+        Arguments:
+            _input (Tensor): Input tensor
+        '''
         new_shape = np.ceil(
             _input.shape / self._subvolume_shape) * self._subvolume_shape
         new_shape = tuple(new_shape.astype('int'))
@@ -39,6 +62,9 @@ class Subject:
 
 
     def load_volume(self):
+        '''
+        Loads subject input data and target.
+        '''
         self._inputs = {}
         self._target = None
         self._volume_shape = None
@@ -47,7 +73,7 @@ class Subject:
             _input = torch.from_numpy(np.array(
                 nipy.load_image(self._path + input_filename).get_data(), dtype='float'))
             if self._extended:
-                _input = self._pad_volume(_input)
+                _input = self._extend_volume(_input)
             if self._volume_shape is None:
                 self._volume_shape = _input.shape
             else:
@@ -61,7 +87,7 @@ class Subject:
             _target = torch.from_numpy(np.array(nipy.load_image(
                 self._path + self._target_filename).get_data(), dtype='int'))
             if self._extended:
-                _target = self._pad_volume(_target)
+                _target = self._extend_volume(_target)
             # check for consistency with input shape
             assert _target.shape == self._volume_shape, \
                 'Target shapes are not consistent with Input shapes for {}'.format(
@@ -69,11 +95,21 @@ class Subject:
             self._target = _target
     
     def generate_nonoverlap_coordinates(self):
+        '''
+        Generates nonoverlap grid.
+        '''
 
         def generate_centered_nonoverlap_1d_grid(length, step):
-            # Calculation of the centered nonoverlap grid
-            # Grid will not cover the whole volume if the multiplier 
-            # of the volume shape is not equal to subvolume shape
+            '''
+            Generates a centered nonoverlap grid.
+
+            Grid will not cover the whole volume if the multiplier 
+            of the volume shape is not equal to subvolume shape.
+
+            ARguments:
+                length (int): volume side length
+                step (int): subvolume side length
+            '''
             return [(c, c + step) for c in range(
                 (length % step) // 2, length - step + 1, step)]
                 
@@ -86,7 +122,14 @@ class Subject:
         self._nonoverlap_coordinates = np.array([[i, j, l] for i in z for j in y for l in x])
 
 
-    def set_truncated_gaussian_coordinate_generator(self, mus=None, sigmas=None):
+    def init_truncated_gaussian_coordinate_generator(self, mus=None, sigmas=None):
+        '''
+        Initiliaze generator for truncated gaussian coordinates.
+
+        Arguments:
+            mus (array of ints): mean values
+            sigmas (array of ints): std values
+        '''
         if mus is None:
             mus = np.array(
                 [self._volume_shape[0] // 2, 
@@ -107,6 +150,9 @@ class Subject:
 
 
     def generate_truncated_gaussian_coordinate(self):
+        '''
+        Samples start and end coordinates for subvolume.
+        '''
         xyz = np.round(self._truncnorm_coordinates.rvs(size=(1, 3))[0]).astype('int')
         xyz_start = xyz - self._half_subvolume_shape
         xyz_end = xyz + self._half_subvolume_shape
@@ -115,34 +161,66 @@ class Subject:
 
 
     def get_input(self):
+        '''
+        Returns inputs.
+        '''
         return self._inputs
 
     
     def get_target(self):
+        '''
+        Returns target.
+        '''
         return self._target
 
 
     def get_nonoverlap_coordinates(self):
+        '''
+        Returns nonoverlap grid.
+        '''
         return self._nonoverlap_coordinates
 
     
     def get_nonoverlap_coordinate(self, index):
+        '''
+        Returns nonoverlap coodtinate by index:
+        
+        Arguments:
+            index (int): index of coordinate
+        '''
         return self._nonoverlap_coordinates[index]
 
     
     def get_volume_shape(self):
+        '''
+        Returns volume shape.
+        '''
         return self._volume_shape
-
-    
-    def get_target(self):
-        return self._target
 
 
     def get_original(self):
+        '''
+        Returns coordinates of original volume.
+        '''
         return self._original
 
 
 class VolumetricDataset(Dataset):
+    '''
+    Encapsulates volumetric dataset.
+
+    Arguments:
+        filename (str or list): file with list of subjects pathes
+        n_subvolumes (int): number of subvolumes to sample
+        subvolume_shape (array of ints): sampled subvolumes shape
+        mus (array of ints): mean values for gaussian sampling
+        sigmas (array of ints): std values for gaussian sampling
+        input_filenames (list of str): filenames of input volumes
+        target_filename (str): filename of target
+        preprocessing (list of functions): functions is used to preprocess input
+        evaluation (Bool): set dataset in evaluation mode
+        extended (Bool): extend volume shape to be dividend of subvolume shape
+    '''
     def __init__(self, 
         filename, n_subvolumes, subvolume_shape, mus=None, sigmas=None, 
         input_filenames=['T1.nii.gz'], target_filename='atlas_full_104.nii.gz', 
@@ -164,6 +242,9 @@ class VolumetricDataset(Dataset):
 
     
     def build(self):
+        '''
+        Builds dataset object by loading data and initilize coordinate sampling.
+        '''
         self._load_volumes()
 
         if self._isTest or self._evaluation:
@@ -173,18 +254,35 @@ class VolumetricDataset(Dataset):
 
 
     def get_paths(self):
+        '''
+        Returns pathes of subjects.
+        '''
         return self._paths
 
 
     def get_element(self, index):
+        '''
+        Returns subject by index.
+        Arguments:
+            index (int): index of subject
+        '''
         return self._dataset[index]
 
 
     def get_all_data(self):
+        '''
+        Returns dataset dictionary with all subjects.
+        '''
         return self._dataset
 
 
     def __getitem__(self, index):
+        '''
+        Samples subvolume.
+
+        Arguments:
+            index (int): index of subvolume
+        '''
         brain_id = index // self._n_subvolumes
         coords_id = index // len(self._dataset.keys())
         
@@ -227,10 +325,16 @@ class VolumetricDataset(Dataset):
 
 
     def __len__(self):
+        '''
+        Provides the size of the dataset.
+        '''
         return self._n_subvolumes * len(self._dataset.keys())
 
 
     def _read_paths(self):
+        '''
+        Reads paths of the subject data.
+        '''
         if isinstance(self._filename, str):
             try:
                 f = open(self._filename, 'r')
@@ -244,18 +348,30 @@ class VolumetricDataset(Dataset):
 
 
     def _nonoverlap_coordinates(self):
+        '''
+        Initilize nonoverlap grid for each subject.
+        '''
         for k in self._dataset.keys():
             self._dataset[k].generate_nonoverlap_coordinates()
             
 
     def _truncnorm_coordinates(self):
+        '''
+        Initiliazes gaussian generators for each subject.
+        '''
         for k in self._dataset.keys():
-            self._dataset[k].set_truncated_gaussian_coordinate_generator(
+            self._dataset[k].init_truncated_gaussian_coordinate_generator(
                mus=self._mus, sigmas=self._sigmas)
 
 
-    def load_job(self, index):
-        subject = Subject(self._paths[index], self._input_filenames, 
+    def _load_job(self, path):
+        '''
+        Creates subject and loads it's data.
+
+        Arguments:
+            path (str): subject diretory path
+        '''
+        subject = Subject(path, self._input_filenames, 
             self._target_filename, self._subvolume_shape, 
             self._preprocessing, self._extended)
         subject.load_volume()  
@@ -263,9 +379,12 @@ class VolumetricDataset(Dataset):
 
 
     def _load_volumes(self):
+        '''
+        Loads subject's data.
+        '''
         self._read_paths()
         self._dataset = {}
         subjects = Parallel(n_jobs=-1)(
-            delayed(self.load_job)(i) for i in progressbar.progressbar(
+            delayed(self._load_job)(self._paths[index]) for i in progressbar.progressbar(
                 range(len(self._paths))))
         self._dataset = {i: s for i, s in enumerate(subjects)}
